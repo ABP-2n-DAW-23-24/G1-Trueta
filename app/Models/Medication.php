458746b5aaf346d39b43dcb60dca9d2e85fb4635
Joan Paneque Domingo <joanpd0@gmail.com>
2024-02-23 19:21:41 +0100

    Fixed all merge problems with the migrations and added medication seeders with all the antibiotics

---
+++ app/Models/Medication.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Medication extends Model
+{
+    protected $fillable = [
+        'name'
+    ];
+}
--- database/migrations/2024_02_23_163050_create_medication_table.php
+++ database/migrations/2024_02_23_163050_create_medication_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('medication', function (Blueprint $table) {
+        Schema::create('medications', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('name');
