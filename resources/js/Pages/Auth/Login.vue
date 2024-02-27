c46dd261f4087f6129f6ea55866b755351f3e661
Aniol Moreno Batlle <amoreno@cendrassos.net>
2024-02-27 18:36:58 +0100

    #40 login with responsive and codesniffer

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -51,8 +51,9 @@ const submit = () => {
                             <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"></path>
                         </g>
                     </svg>
-                    <input 
-                        type="text" 
+                    <input
+                        title="Nom d'usuari"
+                        type="text"
                         class="input" 
                         v-model="form.email"
                         required
@@ -69,7 +70,8 @@ const submit = () => {
                         <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path>
                         <path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path>
                     </svg>        
-                    <input 
+                    <input
+                        title="Contrasenya"
                         type="password" 
                         class="input"
                         v-model="form.password"
@@ -86,10 +88,6 @@ const submit = () => {
 </template>
 
 <style>
-body {
-    background-color: #f5f5f5;
-}
-
 .login {
     display: flex;
     justify-content: center;
@@ -99,13 +97,15 @@ body {
 
 .main {
     display: flex;
-    max-width: 1200px;
     margin: 0;
+    border-radius: 15px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
 }
 
 .image-container {
     position: relative;
-    width: 450px;
+    max-width: 500px;
+    width: 100%;
     overflow: hidden;
 }
 
@@ -138,7 +138,8 @@ body {
     gap: 10px;
     background-color: #ffffff;
     padding: 30px;
-    width: 450px;
+    max-width: 400px;
+    width: 100%;
     border-top-right-radius: 15px;
     border-bottom-right-radius: 15px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -210,7 +211,8 @@ body {
 
 .button-submit {
     margin: 20px 0 10px 0;
-    background-color: #e87d0d;
+    background-color: #b85e00;
+    filter: brightness(125%);
     border: none;
     color: white;
     font-size: 15px;
@@ -223,7 +225,7 @@ body {
 
 .button-submit:hover {
     transition: all 0.3s ease-in-out;
-    background-color: #c36a0c;
+    background-color: #ab5900;
 }
 
 .p {
@@ -251,5 +253,30 @@ body {
 
 .btn:hover {
     border: 1px solid #00599c;
+}
+
+
+/* RESPONSIVE */
+@media (max-width: 768px) {
+    .main {
+        flex-direction: column;
+        border-radius: 0 0 0 0;
+        box-shadow: 1px solid #4648481e;
+        max-width: 350px;
+    }
+
+    .image-container {
+        border-radius: 0 0 0 0;
+        box-shadow: 1px solid #151717;
+    }
+
+    .image-container img:first-child {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .form {
+        border-radius: 0 0 0 0;
+    }
 }
 </style>
