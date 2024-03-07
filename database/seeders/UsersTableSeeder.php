6ce2124a5e0471b2de4b624f48c085ae192a586e
Joan Paneque <85831928+joanpaneque@users.noreply.github.com>
2024-03-07 15:43:07 +0100

    Changed surname to surnames

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,14 +16,14 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->insert([
             'name' => 'admin',
-            'surname' => 'admin',
+            'surnames' => 'admin',
             'email' => 'administrador@trueta.cat',
             'password' => Hash::make('administrador'),
         ]);
 
         DB::table('users')->insert([
             'name' => 'user',
-            'surname' => 'user',
+            'surnames' => 'user',
             'email' => 'user@treuta.cat',
             'password' => Hash::make('user'),
         ]);
