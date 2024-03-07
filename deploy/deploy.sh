919e7f4424f57ba35ddc4fbf836c25fb808c67c7
Luis Arauz <larauz@cendrassos.net>
2024-03-07 19:35:28 +0100

    prova 10

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-rm -f app.tgz 
-cp ./env/env.example .env
+rm -f app.tgz
 mkdir -p -m 775 ./storage/app/public ./storage/framework/cache ./storage/framework/sessions ./storage/framework/testing ./storage/framework/views ./storage/logs  
 (chgrp -R www-data storage ; true)
 (chmod -R g+rwX storage ; true)
-php artisan migrate
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-#XD
+npm update
+composer update
+npm run dev
+php artisan serve
