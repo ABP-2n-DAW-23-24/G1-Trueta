b225e2fd395834107eb7a2a0f4558d4d1ae18754
Luis Arauz <larauz@cendrassos.net>
2024-03-22 15:35:30 +0100

    correcio lang pest

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -1,8 +1,6 @@
 <?php
-
 namespace Tests\Feature\Auth;
 
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
@@ -11,17 +9,39 @@ class LoginTest extends TestCase
 {
     use RefreshDatabase;
     
-
-    public function test_registration_screen_can_be_rendered(): void
+    public function test_login_screen_can_be_rendered()
     {
-        $user = User::factory()->create();
-    
-
-        $this->actingAs($user);
-    
         $response = $this->get('/login');
-    
         $response->assertStatus(200);
     }
 
+    public function test_authenticated_user_can_login()
+    {
+        $this->withoutExceptionHandling(); 
+    
+        $user = User::factory()->create([
+            'email' => 'administrador@trueta.cat',
+            'password' => bcrypt('administrador'), 
+        ]);
+    
+        $response = $this->post('/login', [
+            'email' => 'administrador@trueta.cat',
+            'password' => 'administrador', 
+        ]);
+    
+        $response->assertRedirect('/dashboard');
+        $this->assertAuthenticatedAs($user);
+    }
+    
+    
+
+    public function test_invalid_credentials_cannot_login()
+    {
+        $response = $this->post('/login', [
+            'email' => 'invalid@example.com',
+            'password' => 'wrongpassword',
+        ]);
+        $response->assertSessionHasErrors();
+        $this->assertGuest();
+    }
 }
